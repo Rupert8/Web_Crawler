330724d655b5224e26905562453d4bf4b4f244c4
Rupert8 <antonsavcenko128@gmail.com>
2025-04-19 16:46:16 +0300

    'Initial'

---
+++ src/main/java/main/web_crawler/WebCrawlerApplication.java
@@ -0,0 +1,15 @@
+package main.web_crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCrawlerApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/main/web_crawler/WebCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package main.web_crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
